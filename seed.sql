adaaf10094abbac3124bda9cf55ddf00cf79d4fe
SBALDOCK <stephenbaldock78@gmail.com>
2020-06-16 20:44:48 -0700

    css

--- seed.sql
+++ seed.sql
@@ -1,4 +1,4 @@
-INSERT INTO books(author, title, image_url, description) VALUES ('Herman Melville', 'Moby Dick', 'https://unsplash.com/photos/QnBrjY-nFUs', 'That is one mean whale');
+INSERT INTO books(author, title, image_url, description) VALUES ('Herman Melville', 'Moby Dick', 'placeholder', 'That is one mean whale');
 INSERT INTO books(author, title) VALUES ('Don Delillo', 'White Noise');
 
 SELECT * FROM books;
